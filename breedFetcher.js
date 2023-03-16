04672c5a843c931faff6e1458930639551ebeef6
Mohammad Razaul Islam <shovon231@gmail.com>
2023-03-16 20:23:24 +0000

    updating breedfrethcher file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,24 @@
 const request = require("request");
-let userArg = "";
-process.argv.forEach(function(val, index, array) {
-  userArg = array[2];
-});
-let callback = function(data) {
-  console.log(data);
-};
-const findBreed = function(userArg, callback) {
-  let searchURL = "https://api.thecatapi.com/v1/breeds/search?q=" + userArg;
+
+let catDesc = "";
+
+const fetchBreedDescription = function(breedName, callback) {
+  let searchURL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
 
   request(searchURL, (error, response, body) => {
     if (error) {
       console.log("Error in finding address");
-      callback(error);
+      callback(error, null);
     } else {
       let jsonBody = JSON.parse(body)[0];
-      if (jsonBody.name === userArg) {
-        console.log(jsonBody.description);
+      if (jsonBody.name === breedName) {
+        catDesc = jsonBody.description;
       } else {
-        console.log("The requested breed is not found");
+        catDesc = "The requested breed is not found";
       }
+      callback(null, catDesc);
     }
   });
 };
-findBreed(userArg, callback);
+
+module.exports = { fetchBreedDescription };
